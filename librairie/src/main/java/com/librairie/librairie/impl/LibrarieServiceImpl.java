b5cb51c4a28dbfb7421c066f90e33e942c95e24d
metaxiii <gael.lehchibi@outlook.com>
2020-05-05 00:40:29 +0200

    remove dependencies

--- librairie/src/main/java/com/librairie/librairie/impl/LibrarieServiceImpl.java
+++ librairie/src/main/java/com/librairie/librairie/impl/LibrarieServiceImpl.java
@@ -4,8 +4,7 @@ import com.librairie.librairie.dto.CollectionDto;
 import com.librairie.librairie.model.Livre;
 import com.librairie.librairie.repositories.LibrairieRepository;
 import com.librairie.librairie.service.ILibrairieService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +16,8 @@ import java.util.Optional;
 
 @Service
 @Transactional
+@Slf4j
 public class LibrarieServiceImpl implements ILibrairieService {
-    private static final Logger logger = LogManager.getLogger(LibrarieServiceImpl.class);
 
     @Autowired
     private LibrairieRepository librairieRepository;
@@ -48,8 +47,8 @@ public class LibrarieServiceImpl implements ILibrairieService {
             librairieRepository.save(livre.get());
             return new ResponseEntity<>(true, HttpStatus.ACCEPTED);
         }
-        if (logger.isErrorEnabled())
-            logger.error(String.format("Le livre n'a pas été trouvé avec l'id %s", id));
+        if (log.isErrorEnabled())
+            log.error(String.format("Le livre n'a pas été trouvé avec l'id %s", id));
         return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
     }
 }
