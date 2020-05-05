62b59007cd4c36c3319e4ed62c472b2d96b12430
metaxiii <gael.lehchibi@outlook.com>
2020-05-05 23:23:12 +0200

    update UT for ConfigServerApplication

--- librairie-config-server/src/test/java/com/librairie/configserver/ConfigServerApplicationTests.java
+++ librairie-config-server/src/test/java/com/librairie/configserver/ConfigServerApplicationTests.java
@@ -2,15 +2,16 @@ package com.librairie.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
+@Rollback
 class ConfigServerApplicationTests {
 
     @Test
     void contextLoads() {
-        assertTrue(true);
+        assertDoesNotThrow(() -> ConfigServerApplication.main(new String[]{}));
     }
-
 }
