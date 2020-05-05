9dfcaa2d381d76351cb01767bbea24279b84389f
metaxiii <gael.lehchibi@outlook.com>
2020-05-05 23:28:40 +0200

    update UT and IT, end of gateway module

--- librairie-gateway/src/test/java/com/librairie/gateway/GatewayApplicationTests.java
+++ librairie-gateway/src/test/java/com/librairie/gateway/GatewayApplicationTests.java
@@ -1,4 +1,16 @@
 package com.librairie.gateway;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SpringBootTest
+@Rollback
 class GatewayApplicationTests {
+    @Test
+    void contextLoads() {
+        assertDoesNotThrow(() -> GatewayApplication.main(new String[]{}));
+    }
 }
