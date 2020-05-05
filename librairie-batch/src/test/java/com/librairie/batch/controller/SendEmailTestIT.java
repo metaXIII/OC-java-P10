e8cf96d5b935f3c59d74f66dfcf743bfc264c781
metaxiii <gael.lehchibi@outlook.com>
2020-05-05 18:28:07 +0200

    remove unused import

--- librairie-batch/src/test/java/com/librairie/batch/controller/SendEmailTestIT.java
+++ librairie-batch/src/test/java/com/librairie/batch/controller/SendEmailTestIT.java
@@ -1,13 +1,12 @@
 package com.librairie.batch.controller;
 
-import feign.RetryableException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 @Rollback
