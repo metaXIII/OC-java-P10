975768ecf65309dce184f1b5dcc0f62777d02d74
metaxiii <gael.lehchibi@outlook.com>
2020-05-05 17:53:37 +0200

    add rollback and transactional annotation

--- librairie/src/test/java/com/librairie/librairie/LibrairieApplicationTestsIT.java
+++ librairie/src/test/java/com/librairie/librairie/LibrairieApplicationTestsIT.java
@@ -2,11 +2,16 @@ package com.librairie.librairie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Rollback
+@Transactional
 class LibrairieApplicationTestsIT {
 
     @Test
