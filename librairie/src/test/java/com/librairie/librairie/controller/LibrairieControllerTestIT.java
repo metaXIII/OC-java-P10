841f1cbf65ca25ed3fc428309d6abef49d02a968
metaxiii <gael.lehchibi@outlook.com>
2020-05-16 22:30:19 +0200

    remove unnecessary config

--- librairie/src/test/java/com/librairie/librairie/controller/LibrairieControllerTestIT.java
+++ librairie/src/test/java/com/librairie/librairie/controller/LibrairieControllerTestIT.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 @Rollback
