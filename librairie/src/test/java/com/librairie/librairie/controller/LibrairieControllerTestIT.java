1450e7b66f883c2b7004c2a3b53adbfa2ffb2e95
metaxiii <gael.lehchibi@outlook.com>
2020-05-16 20:06:36 +0200

    implement test for LibrairieControllerTestIT

--- librairie/src/test/java/com/librairie/librairie/controller/LibrairieControllerTestIT.java
+++ librairie/src/test/java/com/librairie/librairie/controller/LibrairieControllerTestIT.java
@@ -1,19 +1,39 @@
 package com.librairie.librairie.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.librairie.librairie.dto.CollectionDto;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 @Transactional
 @Rollback
 class LibrairieControllerTestIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Autowired
     private LibrairieController librairieController;
@@ -57,5 +77,45 @@ class LibrairieControllerTestIT {
     void getStockWithNoExistingId() {
         assertThrows(NumberFormatException.class,
                      () -> librairieController.getStock("aze").getStatusCodeValue());
+    }
+
+
+    @Test
+    public void findAllLibrairieIT() throws Exception {
+        this.mockMvc.perform(get("/api/librairie/findAll")).andDo(print()).andExpect(status().isAccepted());
+    }
+
+    @Test
+    public void findIT() throws Exception {
+        this.mockMvc.perform(post("/api/librairie/find").content(asJsonString(new CollectionDto("","","")))
+                                     .contentType(MediaType.APPLICATION_JSON)
+                                     .accept(MediaType.APPLICATION_JSON))
+                .andDo(print()).andExpect(status().isAccepted());
+    }
+
+    @Test
+    public void findbyIdIT() throws Exception {
+        this.mockMvc.perform(get("/api/librairie/findById/1"))
+                .andDo(print()).andExpect(status().isAccepted());
+    }
+
+    @Test
+    public void reservedIT() throws Exception {
+        this.mockMvc.perform(get("/api/librairie/reserve/1"))
+                .andDo(print()).andExpect(status().isAccepted());
+    }
+
+    @Test
+    public void getStockIT() throws Exception {
+        this.mockMvc.perform(get("/api/librairie/getStock/1"))
+                .andDo(print()).andExpect(status().isAccepted());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
